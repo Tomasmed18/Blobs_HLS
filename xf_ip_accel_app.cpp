d0552b576e01f434e1681b7a957b19d6b5d13591
Tomasmed18 <tomy-medina@hotmail.com>
2020-04-01 22:40:36 -0300

    Added preprocessing functions

--- xf_ip_accel_app.cpp
+++ xf_ip_accel_app.cpp
@@ -2,8 +2,31 @@
 
 #include "imgproc/xf_cvt_color.hpp"
 #include "imgproc/xf_duplicateimage.hpp"
+#include "imgproc/xf_threshold.hpp"
+#include "core/xf_arithm.hpp"
+#include "imgproc/xf_erosion.hpp"
+#include "imgproc/xf_dilation.hpp"
+
+#define ERODE_ITERATIONS 10
+#define ERODE_FILTER_SIZE 7
+unsigned char erode_kernel[ERODE_FILTER_SIZE*ERODE_FILTER_SIZE] ={0,0,0,1,0,0,0,
+												0,0,0,1,0,0,0,
+												0,0,0,1,0,0,0,
+												1,1,1,1,1,1,1,
+												0,0,0,1,0,0,0,
+												0,0,0,1,0,0,0,
+												0,0,0,1,0,0,0};
 
 
+#define DILATE_ITERATIONS 10
+#define DILATE_FILTER_SIZE 11
+unsigned char dilation_kernel[DILATE_FILTER_SIZE*DILATE_FILTER_SIZE] =   {0,0,0,0,0,1,0,0,0,0,0,
+																			0,0,0,0,0,1,0,0,0,0,0,
+																			0,0,0,0,0,1,0,0,0,0,0,
+																			1,1,1,1,1,1,1,1,1,1,1,
+																			0,0,0,0,0,1,0,0,0,0,0,
+																			0,0,0,0,0,1,0,0,0,0,0,
+																			0,0,0,0,0,1,0,0,0,0,0};
 
 void blobs_accel(hls::stream< ap_axiu<24,1,1,1> >& _src,hls::stream< ap_axiu<24,1,1,1> >& _dst)
 {
@@ -12,10 +35,19 @@ void blobs_accel(hls::stream< ap_axiu<24,1,1,1> >& _src,hls::stream< ap_axiu<24,
 
 	 xf::Mat<XF_8UC3, HEIGHT, WIDTH, NPIX_BLOBS> imgInput(HEIGHT, WIDTH); //RGB
 	 xf::Mat<XF_8UC1, HEIGHT, WIDTH, NPIX_BLOBS> img0(HEIGHT, WIDTH); //GRAY
+	 xf::Mat<XF_8UC1, HEIGHT, WIDTH, NPIX_BLOBS> img1(HEIGHT, WIDTH); //GRAY
+	 xf::Mat<XF_8UC1, HEIGHT, WIDTH, NPIX_BLOBS> img2(HEIGHT, WIDTH); //GRAY
+	 xf::Mat<XF_8UC1, HEIGHT, WIDTH, NPIX_BLOBS> img3(HEIGHT, WIDTH); //GRAY
+	 xf::Mat<XF_8UC1, HEIGHT, WIDTH, NPIX_BLOBS> img4(HEIGHT, WIDTH); //GRAY
 	 xf::Mat<XF_8UC3, HEIGHT, WIDTH, NPIX_BLOBS> imgOutput(HEIGHT, WIDTH); //RGB
 
 
 #pragma HLS stream variable=imgInput.data dim=1 depth=1
+#pragma HLS stream variable=img0.data dim=1 depth=1
+#pragma HLS stream variable=img1.data dim=1 depth=1
+#pragma HLS stream variable=img2.data dim=1 depth=1
+#pragma HLS stream variable=img3.data dim=1 depth=1
+#pragma HLS stream variable=img4.data dim=1 depth=1
 #pragma HLS stream variable=imgOutputput.data dim=1 depth=1
 	#pragma HLS dataflow
 
@@ -25,7 +57,12 @@ void blobs_accel(hls::stream< ap_axiu<24,1,1,1> >& _src,hls::stream< ap_axiu<24,
 	xf::rgb2gray<XF_8UC3, XF_8UC1, HEIGHT, WIDTH, XF_NPPC1>(imgInput, img0);
 
 
-
+//	xf::Threshold<XF_THRESHOLD_TYPE_BINARY, XF_8UC1, HEIGHT, WIDTH, NPIX_BLOBS>(img0, img1, 118, 255);
+//	xf::bitwise_not<XF_8UC1, HEIGHT, WIDTH, NPIX_BLOBS>(img1, img2);
+//	xf::erode<XF_BORDER_CONSTANT, XF_8UC1 , HEIGHT, WIDTH, XF_SHAPE_ELLIPSE,
+//		ERODE_FILTER_SIZE, ERODE_FILTER_SIZE, ERODE_ITERATIONS, NPIX_BLOBS>(img2, img3, erode_kernel);
+//	xf::dilate<XF_BORDER_CONSTANT, XF_8UC1 , HEIGHT, WIDTH, XF_SHAPE_ELLIPSE,
+//			DILATE_FILTER_SIZE, DILATE_FILTER_SIZE, DILATE_ITERATIONS, NPIX_BLOBS>(img3, img4, dilation_kernel);
 
 
 	xf::gray2rgb<XF_8UC1, XF_8UC3, HEIGHT, WIDTH, XF_NPPC1>(img0, imgOutput);
